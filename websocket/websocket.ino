3c5edc40370b70bb145e082f072d1109c702167b
Eleo Basili <eleonel.basili@gmail.com>
2024-07-01 16:42:51 +0900

    Improve look and feel of text and animation.

--- websocket/websocket.ino
+++ websocket/websocket.ino
@@ -14,9 +14,7 @@ static lv_disp_draw_buf_t draw_buf;
 static lv_color_t *buf;
 lv_obj_t *label;  // Global label object
 
-void my_disp_flush(lv_disp_drv_t *disp,
-                   const lv_area_t *area,
-                   lv_color_t *color_p) {
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
   uint32_t w = (area->x2 - area->x1 + 1);
   uint32_t h = (area->y2 - area->y1 + 1);
   lcd_PushColors(area->x1, area->y1, w, h, (uint16_t *)&color_p->full);
@@ -27,7 +25,7 @@ static void scroll_anim_cb(void *var, int32_t v) {
   lv_obj_set_y((lv_obj_t *)var, v);
 }
 
-void create_scroll_animation(lv_obj_t *obj, int32_t end, int32_t start, uint32_t duration) {
+void create_scroll_animation(lv_obj_t *obj, int32_t start, int32_t end, uint32_t duration) {
   lv_anim_t a;
   lv_anim_init(&a);
   lv_anim_set_var(&a, obj);
@@ -69,26 +67,25 @@ void setup() {
   disp_drv.flush_cb = my_disp_flush;
   disp_drv.draw_buf = &draw_buf;
   lv_disp_drv_register(&disp_drv);
-  // Screen background
-  //lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x003a57), LV_PART_MAIN);
 
   // Style settings for the label
   static lv_style_t style;
   lv_style_init(&style);
-  lv_style_set_text_font(&style, &lv_font_montserrat_40); // Use custom Montserrat font size 40
-  lv_style_set_text_color(&style, lv_color_white()); // Set text color
-  lv_style_set_bg_color(&style, lv_color_black()); // Set background color
+  lv_style_set_text_font(&style, &lv_font_montserrat_40); // Use Montserrat font size 40
+  lv_style_set_text_color(&style, lv_color_white()); // White text color
+  lv_style_set_bg_color(&style, lv_color_black()); // Black background color
   lv_style_set_pad_all(&style, 5); // Set padding
+  lv_style_set_text_align(&style, LV_TEXT_ALIGN_CENTER); // Center align the text
 
   label = lv_label_create(lv_scr_act());
   lv_obj_add_style(label, &style, 0); // Apply style to label
   lv_label_set_text(label, "This is a long text that will be scrolled up and down automatically. Add more text here to test scrolling functionality. This is a very long text indeed.");
   lv_label_set_long_mode(label, LV_LABEL_LONG_WRAP);  // Break the long lines
-  lv_obj_set_width(label, 535);                       // Adjust the label width to the display width
-  lv_obj_align(label, LV_ALIGN_TOP_LEFT, 0, 0);        // Align the label to the top middle
+  lv_obj_set_width(label, 525);                       // Adjust the label width to the display width
+  lv_obj_align(label, LV_ALIGN_BOTTOM_MID, 0, 0);     // Align the label to the bottom middle
 
   // Create scroll animation
-  create_scroll_animation(label, -50, -lv_obj_get_height(label) + EXAMPLE_LCD_V_RES, 20000); // 20 seconds for a full scroll
+  create_scroll_animation(label, EXAMPLE_LCD_V_RES + 75, -lv_obj_get_height(label), 15000); // 15 seconds for a full scroll
 }
 
 void loop() {
@@ -97,8 +94,8 @@ void loop() {
   if (Serial.available()) {
     String received = Serial.readStringUntil('\n');
     lv_label_set_text(label, received.c_str());
-    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);        // Re-align the label to the top middle
-    create_scroll_animation(label, -50, -lv_obj_get_height(label) + EXAMPLE_LCD_V_RES, 20000); // Restart animation
+    lv_obj_align(label, LV_ALIGN_BOTTOM_MID, 0, 0); // Re-align the label to the bottom middle
+    create_scroll_animation(label, EXAMPLE_LCD_V_RES + 75, -lv_obj_get_height(label), 15000); // Restart scroll animation
   }
   delay(5);
 }

cb70ea5ec62a2580d0fcf218a4a2605b9b81745e
Eleo Basili <eleonel.basili@gmail.com>
2026-09-09 23:25:03 +0900

    Export binaries.

--- webscreen/webscreen_config.h
+++ webscreen/webscreen_config.h
@@ -18,7 +18,7 @@
 #define WEBSCREEN_VERSION_MAJOR 2
 #define WEBSCREEN_VERSION_MINOR 2
 #define WEBSCREEN_VERSION_PATCH 0
-#define WEBSCREEN_VERSION_STRING "2.2.0-dev"
+#define WEBSCREEN_VERSION_STRING "3.0.0-dev"
 
 // ============================================================================
 // HARDWARE CONFIGURATION
